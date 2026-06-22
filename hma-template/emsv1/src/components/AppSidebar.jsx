418006935a9123f9fbd6a718f9896876d105493c
Joel John George <joeljohngeorge2005@gmail.com>
2026-06-22 14:06:38 +0530

    fix: found the difference in the sidebar

--- hma-template/emsv1/src/components/AppSidebar.jsx
+++ hma-template/emsv1/src/components/AppSidebar.jsx
@@ -9,12 +9,9 @@ import {
   CSidebarHeader,
   CSidebarToggler,
 } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
-
 import { AppSidebarNav } from './AppSidebarNav'
 
-import { logo } from 'src/assets/brand/logo'
-import { sygnet } from 'src/assets/brand/sygnet'
+import hmaLogo from 'src/assets/brand/hma-logo.png'
 
 import useRole from '../hooks/useRole'
 
@@ -41,8 +38,20 @@ const AppSidebar = ({ nav = [] }) => {
     >
       <CSidebarHeader className="border-bottom">
         <CSidebarBrand to="/">
-          <CIcon customClassName="sidebar-brand-full" icon={logo} height={32} />
-          <CIcon customClassName="sidebar-brand-narrow" icon={sygnet} height={32} />
+          <img
+            src={hmaLogo}
+            alt="HMA"
+            height={40}
+            className="sidebar-brand-full"
+            style={{ objectFit: 'contain' }}
+          />
+          <img
+            src={hmaLogo}
+            alt="HMA"
+            height={32}
+            className="sidebar-brand-narrow"
+            style={{ objectFit: 'contain' }}
+          />
         </CSidebarBrand>
         <CCloseButton
           className="d-lg-none"
